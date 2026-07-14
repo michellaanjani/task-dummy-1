1d6f3eadebad3c00e3578c919f417a6870feac3a
michellaanjani <elmamoramichel@gmail.com>
2026-07-14 08:25:13 +0700

    Fix .gitignore and re-apply

--- .om_hospital/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from . import patient
-from . import doctor
-from . import sale
-from . import appointment
-from . import prescription
-from . import product
--- .om_hospital/report/__init__.py
+++
@@ -1,3 +0,0 @@
-from . import patient_card_xlsx
-from . import all_patient_report
-from . import patient_appointment_xlsx
